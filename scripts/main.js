071d6e66cdc7f25fff17ecaae3e610f63c1ee27f
ungerandrew2 <ungerandrew2@gmail.com>
2020-01-24 19:07:47 -0600

    whole bunch of sheet

--- scripts/main.js
+++ scripts/main.js
@@ -13,8 +13,9 @@ function myFunction() {
     var navbar = document.getElementById("myNavbar");
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         navbar.className = "w3-bar" + " w3-card-2" + " w3-animate-top" + " w3-white";
-    } else {
-        navbar.className = navbar.className.replace(" w3-card-2 w3-animate-top w3-white", "");
+    }    
+    else {
+        navbar.className = "w3-bar w3-card w3-animate-top w3-white";
     }
 }
 
