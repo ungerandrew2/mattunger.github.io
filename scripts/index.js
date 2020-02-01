7473d8826b7f303c80071cd62783d9fb6fc32f10
ungerandrew2 <ungerandrew2@gmail.com>
2020-02-01 16:14:53 -0600

    test animation

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+	container: document.getElementById('animation'),
+	renderer: 'svg',
+	loop: false,
+	autoplay: false,
+	path: 'scripts/Animation.json'
+});
