e7d7fc70920e44263c245ca5333542c0577ec9a9
ungerandrew2 <ungerandrew2@gmail.com>
2020-02-03 18:28:17 -0600

    Revert "test animation"
    This reverts commit 7473d8826b7f303c80071cd62783d9fb6fc32f10.

--- scripts/index.js
+++
@@ -1,7 +0,0 @@
-var animation = bodymovin.loadAnimation({
-	container: document.getElementById('animation'),
-	renderer: 'svg',
-	loop: false,
-	autoplay: false,
-	path: 'scripts/Animation.json'
-});
