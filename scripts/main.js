8afd6aecd69b38effc4638092604ef8823505d1c
ungerandrew2 <ungerandrew2@gmail.com>
2020-02-03 20:09:39 -0600

    maybeeee?

--- scripts/main.js
+++ scripts/main.js
@@ -12,10 +12,10 @@ window.onscroll = function () { myFunction() };
 function myFunction() {
     var navbar = document.getElementById("myNavbar");
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        navbar.className = "w3-bar" + " w3-card-2" + " w3-animate-top" + " w3-white";
+        navbar.className = "w3-bar" + " w3-card-2" + " w3-animate-top";
     }    
     else {
-        navbar.className = "w3-bar w3-card w3-animate-top w3-white";
+        navbar.className = "w3-bar w3-card w3-animate-top";
     }
 }
 
